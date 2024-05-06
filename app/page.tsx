0ccdbd7c753bd6c5e9a089d5e8bb86a879688fb0
Denis <denislarin2017@gmail.com>
2024-05-06 14:35:32 +0300

    InitApp / NextJS initial app

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-5 text-white bg-gradient-to-tl from-blue-600 via-cyan-500 to-blue-800">
+      <div className="text-3xl tracking-widest flex items-center gap-5" style={{ textShadow: '6px 6px 12px rgba(0, 0, 0, 0.7)' }}>
+        <img src="https://piramid.perfectum.cloud/files/piramid/uploads/company/logo.png" alt="Піраміда-жалюзі" />
+        <h1 className="text-[#241a69]">PIRAMID 2024</h1>
+      </div>
+      <p className="text-2xl opacity-60">Soon...</p>
+    </main>
+
+  );
+}
