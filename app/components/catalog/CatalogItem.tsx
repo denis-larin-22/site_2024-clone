999154175152f4b36ab87acc99da7c9b21cf0570
Denis <denislarin2017@gmail.com>
2024-06-03 11:34:10 +0300

    Updeted pop-up card (desktop ver.)

--- app/components/catalog/CatalogItem.tsx
+++ app/components/catalog/CatalogItem.tsx
@@ -20,14 +20,12 @@ export default function CatalogItem({ catalogItem }: IProps) {
     const [selectedColor, setSelectedColor] = useState<string>(availableColors[0])
 
     const technicalInformation = [
-        { item: "Застосування", info: type },
-        { item: "Колекція", info: collection },
-        { item: "Ширина рулону", info: maxWidth },
         { item: "Затемнення", info: opacity },
         { item: "Водостійкість", info: waterproofnessLevel },
         { item: "Фактура тканини", info: fabricTexture },
         { item: "Склад", info: composition },
         { item: "Гарантія", info: guarantee },
+        { item: "Ширина рулону", info: maxWidth }
     ];
 
     // Additional styles
@@ -83,7 +81,7 @@ export default function CatalogItem({ catalogItem }: IProps) {
                 backdrop="opaque"
                 classNames={{
                     wrapper: "h-fit my-auto",
-                    base: "relative max-w-[998px] min-h-[523px] p-10 bg-[#7E7E7E]/20 backdrop-blur-lg text-white",
+                    base: "relative max-w-[998px] min-h-[523px] p-10 bg-[#FAFAFA] text-t-blue-dark",
                     body: "p-0 flex flex-row items-center justify-between gap-x-[330px]"
                 }}
                 isOpen={isOpen}
@@ -94,7 +92,7 @@ export default function CatalogItem({ catalogItem }: IProps) {
                 <ModalContent>
                     {(onClose) => (
                         <ModalBody>
-                            <CloseButton btnHandler={onClose} className="absolute right-4 top-4" />
+                            <CloseButton btnHandler={onClose} iconColor="#10005B" className="absolute right-4 top-4" />
 
                             <ul className="flex flex-col gap-2.5">
                                 {availableColors.map((color, index) => (
@@ -148,7 +146,7 @@ export default function CatalogItem({ catalogItem }: IProps) {
                                                     loading="lazy"
                                                     className="object-cover"
                                                 />
-                                                <CloseButton btnHandler={onCloseZoomed} iconColor="#1E1E1E" className="absolute top-3 right-3" />
+                                                <CloseButton btnHandler={onCloseZoomed} iconColor="#10005B" className="absolute top-3 right-3 z-50" />
                                             </>
                                         )}
                                     </ModalContent>
@@ -156,8 +154,12 @@ export default function CatalogItem({ catalogItem }: IProps) {
                             </div>
 
                             <section className="w-[472px]">
-                                <div className="bg-white rounded-xl p-[25px]">
-                                    <h5 className="text-[32px] text-t-dark-text mb-[25px]">{name}</h5>
+                                <div>
+                                    <div className={`${openSansFont.className} flex items-center justify-between`}>
+                                        <p className="text-[#AEB1BA] text-xs">{type.toUpperCase()} <span className="text-t-blue-dark">/</span> {collection.toUpperCase()}</p>
+                                        <p className={`${isInStock === "в наявності" ? "text-t-green" : "text-[#FF4242]"}`}>{isInStock}</p>
+                                    </div>
+                                    <h5 className="text-[32px] mt-3 mb-8">{name}</h5>
                                     <div className="w-full flex items-center justify-between">
                                         <div className="flex items-center gap-[15px]">
                                             {specialOffer === undefined ? null : <p className="relative w-[113px] py-1 px-3 flex items-center justify-end text-xs font-bold text-[#F79D15] bg-[#FFEFD1] rounded-full">
@@ -172,18 +174,17 @@ export default function CatalogItem({ catalogItem }: IProps) {
                                                 {label}
                                             </p>
                                         </div>
-                                        <p className={`${openSansFont.className} ${isInStock === "в наявності" ? "text-t-green" : "text-[#FF4242]"}`}>{isInStock}</p>
                                     </div>
                                 </div>
 
-                                <div className={`${openSansFont.className} text-lg font-normal`}>
-                                    <p className="inline-block w-full pb-5 mt-[27px] mb-5 border-b-1 border-[#DDE0E9]">Технічна інформація</p>
+                                <div className={`${openSansFont.className} text-lg font-normal mt-[60px]`}>
+                                    <p className="inline-block w-full pb-5 mb-5 border-b-1 border-[#DDE0E9]">Технічна інформація</p>
 
                                     <ul className="text-lg grid grid-cols-3 gap-x-2 gap-y-[30px]">
                                         {technicalInformation.map((infoItem, index) => (
                                             <li key={index}>
                                                 <p>{infoItem.item}</p>
-                                                <p className="text-sm text-t-pale">{infoItem.info}</p>
+                                                <p className="mt-1 text-sm text-[#AEB1BA]">{infoItem.info}</p>
                                             </li>
                                         ))}
                                     </ul>
@@ -202,8 +203,8 @@ function CloseButton({ className, iconColor = "white", btnHandler }: { className
     return (
         <button className={`w-fit h-fit ${className}`} onClick={btnHandler}>
             <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M18.5318 0.466859C19.0288 0.963795 19.0288 1.76949 18.5318 2.26642L2.26837 18.5299C1.77143 19.0268 0.965742 19.0268 0.468806 18.5299C-0.0281295 18.0329 -0.0281297 17.2273 0.468807 16.7303L16.7323 0.466859C17.2292 -0.0300766 18.0349 -0.0300766 18.5318 0.466859Z" fill={iconColor} />
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M18.5314 18.5275C18.0345 19.0245 17.2288 19.0245 16.7319 18.5275L0.468405 2.26406C-0.028531 1.76713 -0.0285307 0.961434 0.468405 0.464499C0.965341 -0.032437 1.77103 -0.0324372 2.26797 0.464499L18.5314 16.728C19.0284 17.2249 19.0284 18.0306 18.5314 18.5275Z" fill={iconColor} />
+                <path fillRule="evenodd" clipRule="evenodd" d="M18.5318 0.466859C19.0288 0.963795 19.0288 1.76949 18.5318 2.26642L2.26837 18.5299C1.77143 19.0268 0.965742 19.0268 0.468806 18.5299C-0.0281295 18.0329 -0.0281297 17.2273 0.468807 16.7303L16.7323 0.466859C17.2292 -0.0300766 18.0349 -0.0300766 18.5318 0.466859Z" fill={iconColor} />
+                <path fillRule="evenodd" clipRule="evenodd" d="M18.5314 18.5275C18.0345 19.0245 17.2288 19.0245 16.7319 18.5275L0.468405 2.26406C-0.028531 1.76713 -0.0285307 0.961434 0.468405 0.464499C0.965341 -0.032437 1.77103 -0.0324372 2.26797 0.464499L18.5314 16.728C19.0284 17.2249 19.0284 18.0306 18.5314 18.5275Z" fill={iconColor} />
             </svg>
         </button>
     )
