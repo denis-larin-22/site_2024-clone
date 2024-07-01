6e7079697bc35dddfbe67264c0e3e1b7f9c94408
Denis <denislarin2017@gmail.com>
2024-07-01 12:11:53 +0300

    .env

--- app/lib/contentful/client.ts
+++ app/lib/contentful/client.ts
@@ -1,5 +1,8 @@
 const contentful = require('contentful');
 
+console.log(process.env.CONTENTFUL_SPACE_ID);
+
+
 export const client = contentful.createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
